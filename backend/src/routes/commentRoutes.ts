c14d094b9fac98f1e0bae46f361916f45dc3b149
nguyenthaison14404-cyber <nguyenthaison14404@gmail.com>
2026-01-17 07:13:29 +0700

    commit routes and controllers

---
+++ backend/src/routes/commentRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as commentController from "../controllers/commentController";
+import {requireAuth } from "@clerk/express";
+const router = Router();
+
+router.post("/:productId", requireAuth(), commentController.createComment);
+
+router.delete("/:commentId", requireAuth(), commentController.deleteComment);
+
+export default router;
+
